213b26eec690833133c39e8caa9a6404931ef059
jeus <jeus@jeus-datis>
2016-07-13 16:57:38 +0430

    get messages from irc channel and send by producer to kafka topic before work this code have create topic.

---
+++ src/main/java/com/datis/irc/pojo/UserMessages.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datis.irc.pojo;
+
+/**
+ *
+ * @author jeus
+ */
+public class UserMessages {
+
+    public String user;
+    public String login;
+    public String hostName;
+    public String message;
+    public String channel;
+    public Long timestamp;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    
+    
+}
