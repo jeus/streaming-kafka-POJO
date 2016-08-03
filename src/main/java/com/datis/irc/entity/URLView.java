d9902c375c499baaa1901ae5de65870d759b6d36
jeus <alkhandani@gmail.com>
2016-08-03 13:59:25 +0430

    add URLView

---
+++ src/main/java/com/datis/irc/entity/URLView.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datis.irc.entity;
+
+/**
+ *
+ * @author jeus
+ */
+public class URLView {
+
+    private String url = "";
+    private String user = "";
+    private String region = "";
+
+    public URLView() {
+    }
+
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+}
